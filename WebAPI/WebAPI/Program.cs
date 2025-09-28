cf38db90b84d81955840576c9164572d0d336f12
Miguel <miganrobles@gmail.com>
2025-09-28 21:05:20 +0200

    Añadimos cors para controlar que servicios se van a poder comunicar con nuestro webAPI

--- WebAPI/WebAPI/Program.cs
+++ WebAPI/WebAPI/Program.cs
@@ -6,6 +6,17 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Añadimos cors para controlar que servicios se van a poder comunicar con nuestro webAPI
+
+var origenesPermitidos = builder.Configuration.GetValue<string>("OrigenesPermitidos")!.Split(',');
+builder.Services.AddCors(opciones =>
+{
+    opciones.AddDefaultPolicy(politica =>
+    {
+        politica.WithOrigins(origenesPermitidos).AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +26,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Añadimos cors aquí también
+app.UseCors();
 
 app.UseAuthorization();
 
