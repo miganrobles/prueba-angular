d9b6e2e36c5a4adb5712914cb88265b29d5fbc1e
Miguel <miganrobles@gmail.com>
2025-09-29 00:15:48 +0200

    Añadida llamada a servicio WeatherForecast

--- app-de-angular/src/app/app.component.ts
+++ app-de-angular/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { WeatherForecastService } from './weather-forecast.service';
+import { of } from 'rxjs';
+import { timeout, catchError, finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-de-angular';
+  errorMsg = '';
+  loaderVisible = false;
+
+  climas: any[] = [];
+   // 👇 aquí va la inyección clásica de Angular 6
+  constructor(private weatherForecastsService: WeatherForecastService) { 
+    this.loaderVisible = true;
+    this.weatherForecastsService.obtenerClima()
+    .pipe(
+        timeout(111), // si tarda más de 2s => error
+        catchError(err => {
+          this.errorMsg = 'El servicio tardó demasiado o falló.';
+          console.error('Error en API simulada:', err);
+          // Retorna un observable vacío para que 'next' reciba algo seguro
+          return of([]);
+        }),
+        finalize(() => {
+          // siempre se ejecuta
+          this.loaderVisible = false;
+        })
+      )
+    .subscribe(data => {
+      this.climas = data;
+    })
+    ;
+  }
 }
